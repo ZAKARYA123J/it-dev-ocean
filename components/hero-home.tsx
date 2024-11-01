959e800c0037aa01688923c63724693d2a076a64
ds <zakaryabaouali255@gmail.com>
2024-11-01 10:55:06 +0100

    fd

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -1,5 +1,6 @@
 import VideoThumb from "@/public/images/hero-image-01.jpg";
 import ModalVideo from "@/components/modal-video";
+import Link from "next/link";
 
 export default function HeroHome() {
   return (
@@ -40,12 +41,14 @@ export default function HeroHome() {
                   </a>
                 </div>
                 <div data-aos="fade-up" data-aos-delay={600}>
-                  <a
+                  <Link href='/contact'>
+                  <button
                     className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
-                    href="#contact"
+                    
                   >
                     Contact Us
-                  </a>
+                  </button>
+                  </Link>
                 </div>
               </div>
             </div>
