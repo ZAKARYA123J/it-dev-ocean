db43d5c4085a75e8db331f2b4af48afa27e4acea
ds <zakaryabaouali255@gmail.com>
2024-11-07 09:53:35 +0100

    fdf

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -14,14 +14,15 @@ export default function Header() {
   return (
     <header className="z-30 mt-2 w-full md:mt-5">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-gray-700/90 px-3">
+        <div className="relative flex h-14 items-center justify-between  gap-1 sm:gap-3 rounded-2xl bg-gray-700/90 px-3">
           {/* Site branding */}
           <div className="flex flex-1 items-center">
             <Logo />
           </div>
 
           {/* Desktop links */}
-          <ul className="flex flex-1 items-center justify-end gap-3">
+        <ul className="flex flex-1 items-center justify-end gap-0 sm:gap-3">
+
             <li>
               <Link href="/" className={getLinkClassName("/")}>
                 Home
